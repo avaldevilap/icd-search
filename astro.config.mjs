8f3038f2d8816c6b353314138ef02b87c2793348
Alejandro Valdevila Pérez <avaldevilap@gmail.com>
2024-03-21 10:47:06 +0100

    feat: Update API endpoint and add new functionality for Foundation Entity Lookup
    - Updated API endpoint for Foundation Entity Lookup to the latest version.
    - Added functionality to fetch data based on foundationUri.
    - Improved authorization handling and language selection.
    - Rendered the code in a new component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,4 +15,11 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone',
   }),
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
